f885df4a4cb559c2be6b5cea7a63a6f058faa5f3
SuperRavenSn1per <70824779+SuperRavenSn1per@users.noreply.github.com>
2021-09-07 00:00:29 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -3,7 +3,7 @@ apiDir = "apis/"
 term.clear()
 term.setCursorPos(1,1)
 print("Loading apis...")
-for i,file in pairs(fs.list(apiDir) do
+for i,file in pairs(fs.list(apiDir)) do
    os.loadAPI(apiDir.."/"..file)
 end
 shell.run("interface")
