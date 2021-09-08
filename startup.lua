0dc721ba7753ca8238bb9b8edec44b9611cfa040
SuperRavenSn1per <70824779+SuperRavenSn1per@users.noreply.github.com>
2021-09-08 17:40:10 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -6,4 +6,13 @@ print("Loading apis...")
 for i,file in pairs(fs.list(apiDir)) do
    os.loadAPI(apiDir.."/"..file)
 end
+term.clear()
+term.setCursorPos(1,1)
+write("Welcome to ")
+term.setTextColor(colors.yellow)
+write("ReactorOS ")
+term.setTextColor(colors.white)
+print("by SuperRavenSn1per")
+xText.centerWrite(" ReactorOS ", colors.white, true, colors.yellow, 0)
+sleep(5)
 shell.run("interface")
