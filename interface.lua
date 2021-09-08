e3ceccfea0a06d407aebd234f26a0736682408a4
SuperRavenSn1per <70824779+SuperRavenSn1per@users.noreply.github.com>
2021-09-08 17:39:22 -0400

    Update interface.lua

--- interface.lua
+++ interface.lua
@@ -5,15 +5,6 @@ print("Getting connection...")
 
 if peripheral.find("BigReactors-Reactor") then
     reactor = peripheral.find("BigReactors-Reactor")
-    term.clear()
-    term.setCursorPos(1,1)
-    write("Welcome to ")
-    term.setTextColor(colors.yellow)
-    write("ReactorOS ")
-    term.setTextColor(colors.white)
-    print("by SuperRavenSn1per")
-    xText.centerWrite(" ReactorOS ", colors.white, true, colors.yellow, 0)
-    sleep(5)
     while true do
         sleep(0.1)
         isActive = reactor.getActive()
